c69fc1b82fddb461bc25cfc64ba344eed84fb65d
AbdullahFah7 <tu4002658@taibahu.edu.sa>
2023-09-27 12:04:20 +0300

    init

---
+++ src/horrorMovieSelector/HorrorMovieSelector.java
@@ -0,0 +1,28 @@
+package horrorMovieSelector;
+import java.util.Random;
+public class HorrorMovieSelector {
+
+	public static void main(String[] args) {
+		String[] horrorMovies = {
+	            "The Shining",
+	            "The Exorcist",
+	            "Psycho",
+	            "Hereditary",
+	            "Get Out"
+	        };
+
+	        // Create a random number generator
+	        Random random = new Random();
+
+	        // Generate a random index within the array bounds
+	        int randomIndex = random.nextInt(horrorMovies.length);
+
+	        // Get the randomly selected horror movie
+	        String randomMovie = horrorMovies[randomIndex];
+
+	        // Print the randomly selected horror movie
+	        System.out.println("Randomly selected horror movie: " + randomMovie);
+
+	}
+
+}
